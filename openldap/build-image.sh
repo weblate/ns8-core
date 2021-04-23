a40de49e714057aff9ecc5d8d6d0507789c1265e
Giacomo Sanchietti <giacomo.sanchietti@nethesis.it>
2021-04-23 11:39:07 +0200

    Add openldap

---
+++ openldap/build-image.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+set -e
+messages=("Publish the image with:" "")
+repobase="ghcr.io/nethserver"
+
+reponame="openldap"
+container=$(buildah from scratch)
+
+buildah add "${container}" imageroot /
+buildah config --entrypoint=/ "${container}"
+buildah commit "${container}" "${repobase}/${reponame}"
+messages+=(" buildah push ${repobase}/${reponame} docker://${repobase}/${reponame}:latest")
+
+#
+#
+#
+
+printf "%s\n" "${messages[@]}"
